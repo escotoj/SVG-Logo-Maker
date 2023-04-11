75419c14f31bbdfac6e6185473d74b29172ac4cd
escotoj <j.escoto14@gmail.com>
2023-04-11 13:07:19 -0700

    added all the variables for text and shapes in the shapes file, the require is throwing an error

---
+++ README.md
@@ -0,0 +1,26 @@
+# SVG Generator
+  
+  ## Description
+  to create a command line app that will render an image and save it as an SVG file
+  
+  ## Table of Contents
+  - [Installation](#Installation)
+  - [Credits](#Credits)
+  - [License](#License)
+  - [Contribute](#Contribute)
+  - [Questions](#Questions)
+  
+  ## Installation
+  node.js and you need inquierer, and jest
+  
+  ## License
+  
+   [Functionality Video](https://drive.google.com/file/d/1d8D6xatXV4n0V-omAop8MQRPTvfL1Fi5/view)
+  
+  ## Credits
+  Jose Escoto
+  
+  ## Questions
+  For more questions please contact below: 
+  Email: j.escoto@gmail.com 
+  GitHub: escotoj
---
+++ index.js
@@ -0,0 +1,70 @@
+const inquirer = require('inquirer');
+const fs = require('fs')
+
+// include the shapes js. 
+
+inquirer.prompt([
+    {
+      type: 'input',
+      message: 'Please enter text to be displayed with logo?',
+      name: 'text',
+    },
+    {
+      type: 'input',
+      message: 'What color will the text be?',
+      name: 'textcolor',
+    },
+    {
+      type: 'list',
+      message: 'What shape would you like your image to have?',
+      name: 'shapes',
+      choices: [square, triangle, circle],
+    },
+    {
+      type: 'input',
+      message: 'What color do you want this shape to be?',
+      name: 'shapecolor',
+    }
+  ]).then((answers) => {
+    console.log(answers);
+    const svgImg = template(answers); // create function for template 
+    fs.writeFile('logo.svg', svgImg, error =>
+        error,
+        console.log('Generated logo.svg!')
+    );
+});
+
+
+// ____________ renders svg, has a for-loop and does so with only circles, does not use inquirer. ________________________
+// let centerX = 15
+// let centerY = 15
+// let radius = 10
+// let style = "fill:rgb(200,200,255);"
+
+// const fs = require('fs')
+
+// let svg = ""
+// svg += '<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n'
+// svg += '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n'
+// svg += '<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">\n'
+
+// for (let x = 0; x < 12; x++) {
+//     svg += `\t<circle cx="${centerX+(x*((radius*2)+5))}" cy="${centerY}" r="${radius}" style="${style}"/>\n`
+// }
+
+
+// svg += '</svg>'
+
+// fs.writeFile('testSVG.svg', svg, (err) => {  
+//     // throws an error, you could also catch it here
+//     if (err) throw err;
+
+//     // success case, the file was saved
+//     console.log('SVG written!');
+// });
+
+
+// ------------ THIS IS HOW THE ANSWERS ARE INJECTED, using if else
+// return `<circle cx="150" cy="100" r="80" fill="${this.color}" /
+
+
